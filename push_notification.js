919d7391e3ade7be25e05a81e46b297ac8ce4e43
Ethan Lee <33e.lee@gmail.com>
2022-08-21 23:54:19 -0700

    Add exp backoff retry and delay

--- push_notification.js
+++ push_notification.js
@@ -1,9 +1,14 @@
 import axios from "axios"
+import axiosRetry from "axios-retry"
 
 import { getAccessToken } from "./authentication.js"
 
 const PROJECT_ID = "metamorph-2f9b7"
-const FIREBASE_PUSH_NOTIF_URL = `https://fcm.googleapis.com/v1/projects/${PROJECT_ID}/messages:send`
+const FIREBASE_PUSH_NOTIF_URL = `https://fcm.googleapis.com/v1/projects/${PROJECT_ID}/messages:sen`
+
+axiosRetry(axios, { retries: 3, retryDelay: axiosRetry.exponentialDelay, retryCondition: (error) => {
+    return error.response.status === 503 || error.response.status === 429
+} })
 
 function buildCommonMessage(topic, title, body) {
     return {
@@ -37,10 +42,10 @@ export function sendWebOrder(webOrderDetails) {
             commonMessage,
             options
         ).then((response) => {
-            console.log(response.status)
-            console.log(response.data)
+            console.log("Successfully sent push notif")
         }, (error) => {
-            console.log(error)
+            console.log("Error sending push notif")
+            return error
         })
     })
 }
